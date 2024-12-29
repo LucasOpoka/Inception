b1767d5fe2da225749eea1d244d0fe22e9a60444
Lucas Opoka <lucasopoka@icloud.com>
2024-12-29 19:35:08 +0200

    Ported mariadb container to alpine

--- sources/requirements/mariadb/mariadb_setup.sh
+++ sources/requirements/mariadb/mariadb_setup.sh
@@ -1,17 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
-service mariadb start
+if [ ! -d "/var/lib/mysql/mysql" ]; then
 
-mariadb -u root << EOF
-CREATE DATABASE IF NOT EXISTS $MDB_NAME;
-CREATE USER IF NOT EXISTS '$MDB_USER'@'%' IDENTIFIED BY '$MDB_USER_PWD';
-GRANT ALL PRIVILEGES ON $MDB_NAME.* TO '$MDB_USER'@'%' IDENTIFIED BY '$MDB_USER_PWD';
-GRANT ALL PRIVILEGES ON $MDB_NAME.* TO 'root'@'%' IDENTIFIED BY '$MDB_ROOT_PWD';
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MDB_ROOT_PWD');
+	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+	
+	sleep 1
+
+	mysqld --bootstrap --datadir=/var/lib/mysql --user=mysql << EOF 
+USE mysql;
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MDB_ROOT_PWD';
+CREATE DATABASE $MDB_NAME;
+CREATE USER '$MDB_USER'@'%' IDENTIFIED by '$MDB_USER_PWD';
+GRANT ALL PRIVILEGES ON $MDB_NAME.* TO '$MDB_USER'@'%';
+
+FLUSH PRIVILEGES;
 EOF
 
-sleep 5
+sleep 1
 
-service mariadb stop
+fi
 
-exec $@
+mysqld_safe
